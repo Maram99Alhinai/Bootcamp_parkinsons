afb15f2162f19e6e7b6d1343d0830caa27a62b20
MawadaAlabri <mooodi1396@gmail.com>
2023-09-06 13:16:43 +0400

    add 2 ways

--- model.py
+++ model.py
@@ -8,14 +8,20 @@ from skimage.morphology import closing, disk
 from utils.process_images import *
 from utils.process_data import *
 import pickle
+import joblib
 
 model = Blueprint('model', __name__)
+
+@model.route('/', methods=['GET', 'POST'])
+def home():      
+    return render_template("home.html")
+
 
 with open('rf_model.pickle', 'rb') as model_file:
     loaded_rf_model = pickle.load(model_file)
 
-@model.route('/', methods=['GET', 'POST'])
-def home():
+@model.route('/try2', methods=['GET', 'POST'])
+def pred2():
     if request.method == 'POST':
         image = request.files.get('image')
         if image:
@@ -24,6 +30,7 @@ def home():
             def process_single_image(image_path):
                 # Load and preprocess the image
                 img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+                img = cv2.resize(img, (512, 242))
                 thresh_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
                 clean_img = closing(thresh_img > 0, disk(1))
 
@@ -68,6 +75,46 @@ def home():
 
             processed_df = process_single_image(new_image_path)
             predictions = loaded_rf_model.predict(processed_df )
-            return render_template("home.html", prediction=predictions)
+            return render_template("pred2.html", prediction=predictions)
             
-    return render_template("home.html")
+    return render_template("pred2.html")
+
+
+loaded_model = joblib.load('logistic_regression_model.pkl')
+@model.route('/try1', methods=['GET', 'POST'])
+def pred1():
+    if request.method == 'POST':
+        MDVP1 = request.form.get('MDVP:Fo')
+        MDVP2 = request.form.get('MDVP:Flo')
+        MDVP3 = request.form.get('MDVP:Jitter')
+        MDVP4 = request.form.get('MDVP:Jitter(Abs)')
+        MDVP5 = request.form.get('MDVP:Shimmer')
+        RPDE = request.form.get('RPDE')
+        DFA = request.form.get('DFA')
+        spread1 = request.form.get('spread1')
+        spread2 = request.form.get('spread2')
+        D2 = request.form.get('D2')
+        PPE = request.form.get('PPE')
+
+        data = {
+        'MDVP:Fo(Hz)': [float(MDVP1)],
+        'MDVP:Flo(Hz)': [float(MDVP2)],
+        'MDVP:Jitter(%)': [float(MDVP3)],
+        'MDVP:Jitter(Abs)': [float(MDVP4)],
+        'MDVP:Shimmer': [float(MDVP5)],
+        'RPDE': [float(RPDE)],
+        'DFA': [float(DFA)],
+        'spread1': [float(spread1)],
+        'spread2': [float(spread2)],
+        'D2': [float(D2)],
+        'PPE': [float(PPE)],}
+        input_series = pd.DataFrame(data).squeeze()
+        predicted_class = loaded_model.predict([input_series])
+        if predicted_class == 0:
+            predictions="Healthy"
+            return render_template("pred1.html",prediction=predictions)
+        else:
+            predictions='You May have Parkinson '
+            return render_template("pred1.html",prediction=predictions)
+          
+    return render_template("pred1.html")
